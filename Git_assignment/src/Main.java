c92419aa69c3acc3b4c37042d79cec1a81418f31
Ahmad09988 <Ahmada.salim98@gmail.com>
2022-11-13 18:36:37 +0200

    this my update on the code.

--- Git_assignment/src/Main.java
+++ Git_assignment/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("This is Abdullah Qtaishat, Welcome to get assignment");
+        System.out.println("This is Abdullah Qtaishat, Welcome to git assignment");
+        System.out.println("This is Ahmad Salim, Welcome to git assignment.\n modifing the original code and push it as branch to the repository");
+
+
     }
 
 }
