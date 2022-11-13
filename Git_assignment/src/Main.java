c31fc5aaea47435ee0d87f3b7132115a3dd85353
aalqtais <abdullahqtaishat1@gmail.com>
2022-11-13 19:45:50 +0300

    Modifying through Abdullah-Branch

--- Git_assignment/src/Main.java
+++ Git_assignment/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("This is Abdullah Qtaishat, Welcome to git assignment");
         System.out.println("This is Ahmad Salim, Welcome to git assignment.\n modifing the original code and push it as branch to the repository");
+        System.out.println(" Hello Ahmad This is Abdullah Qtaishat again, I am modifying the code again through branch 2");
 
 
     }
